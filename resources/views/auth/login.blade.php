a7eef98c36e4bc470088139a9c1067592a214109
Johnson Sebire <johnson@manifestghana.com>
2021-10-17 01:23:41 +0000

    Replaced placeholder email for auth page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,7 +22,7 @@
     
     <div class="form-group">
        <!-- <label for="email">Email</label> -->
-        <input type="email" class="form-control" id="email" placeholder="enninful@manifestghana.com" name="email" required autofocus>
+        <input type="email" class="form-control" id="email" placeholder="info@yvedigital.com" name="email" required autofocus>
     </div>
 
     <div class="form-group">
