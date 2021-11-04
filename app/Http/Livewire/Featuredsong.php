3350fc4bc14b4189ae2be9b3372c0ef5ab61343e
Johnson Sebire <johnson@manifestghana.com>
2021-11-04 10:10:24 +0000

    Fixed Bug 0 Offset for Featured Release Module

--- app/Http/Livewire/Featuredsong.php
+++ app/Http/Livewire/Featuredsong.php
@@ -19,17 +19,30 @@ class Featuredsong extends Component
     public function render()
     {
         $user=Auth::user(); 
-        $releaseInfo=Release::where('username', $user->username)->latest()->get(); 
+        $releaseInfo=Release::where('username', $user->username)->latest()->get();
+        if($releaseInfo->count()>0){
+            
         $channel=$releaseInfo[0]['']; 
-        
         $this->releaseType=$releaseInfo[0]['genre']; 
         $this->releaseTitle=$releaseInfo[0]['release_name']; 
         $this->artistName=$user->name; 
         $this->featuredChannel= ''; 
         $this->featuredImage=$releaseInfo[0]['cover_art'];
         $this->releaseDate='2021-01-19'; 
+        } 
 
+        else{
 
+            $channel=''; 
+        
+            $this->releaseType="N/A"; 
+            $this->releaseTitle="No release"; 
+            $this->artistName=$user->name; 
+            $this->featuredChannel=''; 
+            $this->featuredImage="";
+            $this->releaseDate=""; 
+        }
+        
         return view('livewire.featuredsong');
     }
 }
